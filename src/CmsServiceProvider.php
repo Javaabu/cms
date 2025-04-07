8cc8b357df7bed7720a2a1827c7f972741937529
WovenCoast <wovencoast14782@techie.com>
2025-04-07 15:11:00 +0500

    Initial commit

---
+++ src/CmsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Javaabu\Cms;
+
+use Illuminate\Support\ServiceProvider;
+
+class CmsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        // declare publishes
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/cms.php' => config_path('cms.php'),
+            ], 'cms-config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // merge package config with user defined config
+        $this->mergeConfigFrom(__DIR__ . '/../config/cms.php', 'cms');
+    }
+}
